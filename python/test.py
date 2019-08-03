af93f78f9024066e82742c68108a067c56d98a27
Gilles Poncelet <53543187+Gilles-Poncelet@users.noreply.github.com>
2019-08-03 22:54:35 +0200

    Linking Louis' work to mine

--- python/test.py
+++ python/test.py
@@ -4,25 +4,9 @@ import re
 
 codes = ['LELEC1310', 'LELEC1360', 'LINMA1510', 'LMECA1321', 'LMECA1100', 'LFSAB1508']
 projectID = 2
-weeks = [21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
+weeks = [21, 22, 23, 24, 25, 26, 27, 28, 29, 32, 33, 34, 35]
 
-d = getCoursesFromCodes(codes, weeks, projectID)
-print(d)
+a = getCoursesFromCodes(codes, weeks, projectID)
 
-
-# convertir les informations de getCoursesFromCodes en heure de début - heure de fin
-duration = '2h'
-date = '17/05/2019'
-time = '14h00'
-
-tdebut = datetime.strptime(date+'-'+time, '%d/%m/%Y-%Hh%M')
-h, m = [0 if x is '' else int(x) for x in duration.split('h')]
-dt = timedelta(hours=h, minutes=m)
-tfin = tdebut + dt
-
-# Obtenir le numero de la semaine (S1, S2, etc)
-week = tdebut.isocalendar()[1]
-
-print(tdebut)
-print(tfin)
-print(week)
+for course in a:
+    course.scheduling()
