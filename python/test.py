71cb2fac084db4b47d5a6c687410b1b245b9e09c
Gilles Poncelet <53543187+Gilles-Poncelet@users.noreply.github.com>
2019-08-04 00:22:38 +0200

    Fix

--- python/test.py
+++ python/test.py
@@ -6,7 +6,10 @@ codes = ['LELEC1310', 'LELEC1360', 'LINMA1510', 'LMECA1321', 'LMECA1100', 'LFSAB
 projectID = 2
 weeks = [21, 22, 23, 24, 25, 26, 27, 28, 29, 32, 33, 34, 35]
 
-a = getCoursesFromCodes(codes, weeks, projectID)
+cm, tp = getCoursesFromCodes(codes, weeks, projectID)
 
-for course in a:
+for course in tp:
     course.scheduling()
+
+print(len(tp))
+print(len(cm))
