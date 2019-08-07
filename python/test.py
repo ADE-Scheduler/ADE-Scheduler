d36b3d2e22464a8a2b15c37fa9994e9f73cd7cdb
Gilles Poncelet <53543187+Gilles-Poncelet@users.noreply.github.com>
2019-08-07 13:14:00 +0200

    example: my Q6

--- python/test.py
+++ python/test.py
@@ -8,12 +8,14 @@ codes = ['LELEC1310', 'LELEC1360', 'LINMA1510', 'LMECA1321', 'LMECA1100', 'LFSAB
 projectID = 2
 weeks = [21, 22, 23, 24, 25, 26, 27, 28, 29, 32, 33, 34, 35, 36]
 
-c = getCoursesFromCodes(codes, Q2+Q3, projectID)
-best, score = compute(c)
-
 cal = Calendar()
-for event in best:
-    cal.events.add(event)
+c = getCoursesFromCodes(codes, Q2+Q3, projectID)
+for week in range(4, 27):
+    best, score = compute(c, week)
+    if score > 0:
+        print('Probleme avec la semaine numero '+str(week)+', score de: '+str(score))
+    for event in best:
+        cal.events.add(event)
 
 with open('my.ics', 'w') as f:
     f.writelines(cal)
