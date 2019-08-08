14d07a0264012878a360315c40768c64b8daa165
Gilles Poncelet <53543187+Gilles-Poncelet@users.noreply.github.com>
2019-08-09 00:04:47 +0200

    Thread + some descriptions

--- python/computation.py
+++ python/computation.py
@@ -1,14 +1,44 @@
 from itertools import combinations, product
 from event import overlappingTime
+from concurrent.futures import ThreadPoolExecutor
 import math
 
 
+def parallel_compute(courses, forbiddenTimeSlots=None, max_workers=53):
+    """
+    Calls the compute() function for all weeks using parallel programming
+    """
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        futures = [executor.submit(compute, *(courses, i, forbiddenTimeSlots)) for i in range(53)]
+        executor.shutdown(wait=True)
+
+    return [future.result()[0] for future in futures]
+
+
 def compute(courses, week, forbiddenTimeSlots=None):
+    """
+    Generates all the possible schedules for a given week.
+    Then evaluates all those possibilities to pick the best one(s).
+    Parameters:
+    -----------
+    courses: list of course.Course
+        The different courses to be added to the schedule
+    week: int
+        The number of the to-be-scheduled week
+    forbiddenTimeSlots: list of event.CustomEvents
+        The slots that are marked as "busy" by the user
+    Returns:
+    --------
+    best: list of event.CustomEvents
+        The best schedule according to the evaluation function (costFunction())
+    best_score: int
+        The score of said best schedule
+    """
     # List of all events in form of : [[ELEC TP1, ELEC TP2], [ELEC CM], [MATH TP1, MATH TP2, MATH TP3], ...]
-    s = map(iter, filter(lambda e: len(e) != 0, sum((course.getweek(week) for course in courses), ())))
+    all_events = map(iter, filter(lambda e: len(e) != 0, sum((course.getweek(week) for course in courses), ())))
 
     # All possible weeks by selecting one element in each list of the list 's'
-    perm = product(*s)
+    perm = product(*all_events)
 
     # Selecting the best possible schedule
     best_score = math.inf
@@ -25,8 +55,16 @@ def compute(courses, week, forbiddenTimeSlots=None):
 
 def costFunction(weekEvents, forbiddenTimeSlots=None):
     """
-    weekEvents : array of events.CustomEvent - contains all the events of one week
-    forbiddenTimeSlots : array of events.CustomEvent - contains all the time slots you want to be free
+    Function that evaluates the "quality" of a given schedule based on diverse parameters
+    Parameters:
+    -----------
+    weekEvents : array of event.CustomEvent
+        The schedule that is to be evaluated (corresponding to a week)
+    forbiddenTimeSlots: list of event.CustomEvents
+        The slots that are marked as "busy" by the user
+    Returns:
+    --------
+    int, the "cost" of this particular schedule
     """
     # do a n^2 comparison for all overlaps
     p = sum(overlappingTime(*e) for e in combinations(weekEvents, 2))
--- python/test.py
+++ python/test.py
@@ -5,17 +5,23 @@ from ics import Calendar
 
 codes = ['LELEC1310', 'LELEC1360', 'LINMA1510', 'LMECA1321', 'LMECA1100', 'LFSAB1508']
 codes_master = ['LELEC2660', 'LELEC2811', 'LMECA2755', 'LELEC2313', 'LELEC2531', 'LMECA2801', 'LELME2002']
-codes_info = ['lelec2531','lingi2241','lingi2255','lingi2261','lingi2266','lingi2142']
-projectID = 2   # = 9 pour 2019-2020 !
+codes_info = ['lelec2531', 'lingi2241', 'lingi2255', 'lingi2261', 'lingi2266', 'lingi2142']
+projectID = 2  # = 9 pour 2019-2020 !
 
 cal = Calendar()
-c = getCoursesFromCodes(codes_master, Q1+Q2+Q3, 9)
-for week in range(53):
-    best, score = compute(c, week)
-    if score > 0:
-        print('Probleme avec la semaine numero '+str(week)+', score de: '+str(score))
-    for event in best:
+c = getCoursesFromCodes(codes_info, Q1 + Q2 + Q3, 9)
+
+year = parallel_compute(c)
+for week in year:
+    for event in week:
         cal.events.add(event)
+
+# for week in range(53):
+#     best, score = compute(c, week)
+#     if score > 0:
+#         print('Probleme avec la semaine numero '+str(week)+', score de: '+str(score))
+#     for event in best:
+#         cal.events.add(event)
 
 with open('my.ics', 'w') as f:
     f.writelines(cal)
