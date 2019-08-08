05ffe192a01d9fbd8c6e79634139eed93cdbcffc
louisna <louisnavarre@hotmail.com>
2019-08-08 23:06:29 +0200

    CSS, JS, fonts and index added (idea)

--- webapp/app.py
+++ webapp/app.py
@@ -1,13 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 
 def index():
-
- return 'Hello world'
+    return render_template('index.html')
 
 if __name__ == '__main__':
-
- app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
