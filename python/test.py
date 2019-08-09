c2e131bb70cd5775c944778040b52627548be3eb
louisna <louisnavarre@hotmail.com>
2019-08-09 14:12:57 +0200

    jinja added

--- python/test.py
+++ python/test.py
@@ -5,11 +5,11 @@ from ics import Calendar
 
 codes = ['LELEC1310', 'LELEC1360', 'LINMA1510', 'LMECA1321', 'LMECA1100', 'LFSAB1508']
 codes_master = ['LELEC2660', 'LELEC2811', 'LMECA2755', 'LELEC2313', 'LELEC2531', 'LMECA2801', 'LELME2002']
-codes_info = ['lelec2531', 'lingi2241', 'lingi2255', 'lingi2261', 'lingi2266', 'lingi2142']
+codes_info = ['lelec2531', 'lingi2241', 'lingi2255', 'lingi2261', 'lingi2266', 'lfsab2351']
 projectID = 2  # = 9 pour 2019-2020 !
 
 cal = Calendar()
-c = getCoursesFromCodes(codes_master, Q1 + Q2 + Q3, 9)
+c = getCoursesFromCodes(codes_info, Q1 + Q2 + Q3, 9)
 
 year = parallel_compute(c)
 for week, score in year:
