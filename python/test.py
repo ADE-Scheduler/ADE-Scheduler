9e93b344c049c04ba75535b615444e89b769d982
jeertmans <jeertmans@icloud.com>
2019-08-09 16:31:25 +0200

    proposition amélioration

--- python/test.py
+++ python/test.py
@@ -7,13 +7,15 @@ from time import time
 codes = ['LELEC1310', 'LELEC1360', 'LINMA1510', 'LMECA1321', 'LMECA1100', 'LFSAB1508']
 codes_master = ['LELEC2660', 'LELEC2811', 'LMECA2755', 'LELEC2313', 'LELEC2531', 'LMECA2801', 'LELME2002']
 codes_info = ['lelec2531', 'lingi2241', 'lingi2255', 'lingi2261', 'lingi2266', 'lfsab2351']
+codes_q5 = ['langl1873', 'lelec1530', 'lelec1755', 'lepl2351', 'lfsab1105', 'lmeca1451', 'lmeca1855', 'lmeca1901']
 projectID = 2  # = 9 pour 2019-2020 !
 
 cal = Calendar()
 t = time()
-c = getCoursesFromCodes(codes_master, Q1 + Q2 + Q3, 9)
+c = getCoursesFromCodes(codes_q5, Q1 + Q2 + Q3, 9)
 print('Time elapsed for fetching :', time()-t)
 
+"""
 for course in c:
         t = time()
         # Les 4 fonctionnent, les deux meilleurs étant les deux premiers
@@ -22,14 +24,14 @@ for course in c:
         #print(course.name, course.getSummary(weeks=range(53)))
         #print(course.name, course.getSummary(weeks=[3,4,5,6,7,8,9,10,25,26,27,28,29,30,31,33,35,40,45,50]))
         print('Time elapsed :', time()-t)
-
 """
+
 year = parallel_compute(c)
 for week, score in year:
     for event in week:
         cal.events.add(event)
         
-"""
+
 
 # for week in range(53):
 #     best, score = compute(c, week)
@@ -38,7 +40,6 @@ for week, score in year:
 #     for event in best:
 #         cal.events.add(event)
 
-"""
+
 with open('my.ics', 'w') as f:
     f.writelines(cal)
-"""
