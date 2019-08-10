618386b8e1e9293255e18fd198e8f648a898dace
Gilles Poncelet <gillesponcelet98@gmail.com>
2019-08-10 18:19:16 +0200

    nothing

--- webapp/app.py
+++ webapp/app.py
@@ -1,16 +1,25 @@
 from flask import Flask, render_template
+from flask import jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('calendar.html')
+    # event = jsonify(title='MAY 2',
+    #                 start='2019-08-11',
+    #                 end='2019-08-12')
+    title = 'MAY 2'
+    start = '2019-08-11'
+    end = '2019-08-12'
+    return render_template('calendar.html', **locals())
+
 
 # To be chosed
 @app.route('/tobecontinued')
 def continued():
     return render_template('continued.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
