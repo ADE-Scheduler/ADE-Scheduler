2e688411ce40b158d7b537818ddbc75ae0aca473
Louis Navarre <louisnavarre@hotmail.com>
2019-08-10 10:08:57 +0200

    new route and I dont know any more

--- webapp/app.py
+++ webapp/app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template('calendar.html')
 
+# To be chosed
+@app.route('/tobecontinued')
+def continued():
+    return render_template('continued.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
