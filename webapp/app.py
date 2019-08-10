d552aaffc741d90dabcae7502f2e2123315d70cb
Gilles Poncelet <53543187+Gilles-Poncelet@users.noreply.github.com>
2019-08-10 23:14:07 +0200

    cleaner

--- webapp/app.py
+++ webapp/app.py
@@ -1,16 +1,14 @@
 from flask import Flask, render_template
 import json
-
 import sys
 
+# BACK-END FILES
 sys.path.insert(1, '../python')
 from ade import getCoursesFromCodes
 from static_data import Q1, Q2
 from computation import parallel_compute
 
 app = Flask(__name__)
-
-basic_context = {'t': 'Gilles', 's': '2019-08-11', 'e': '2019-08-12'}
 
 codes_master = ['LELEC2660', 'LELEC2811', 'LMECA2755', 'LELEC2313', 'LELEC2531', 'LMECA2801', 'LELME2002']
 c = getCoursesFromCodes(codes_master, Q1 + Q2, 9)
@@ -21,13 +19,13 @@ for week, score in year:
     for event in week[0]:
         temp = {'start': str(event.begin), 'end': str(event.end), 'title': event.name}
         to_send.append(temp)
-        i += 1
-        print(i)
+
 
 @app.route('/')
 def index():
     data = to_send
     return render_template('calendar.html', data=json.dumps(data))
+    # json.dumps creates json objects: 1 per list element !
 
 
 # To be chosed
