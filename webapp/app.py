2a52f34182f08957ec20efe2841cafa76119af56
Gilles Poncelet <53543187+Gilles-Poncelet@users.noreply.github.com>
2019-08-10 19:25:42 +0200

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -2,18 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-basic_context = {}
-basic_context['name'] = 'Gilles'
+basic_context = {'name': 'Gilles'}
+
 
 @app.route('/')
 def index():
     context = basic_context
     return render_template('calendar.html', **context)
 
+
 # To be chosed
 @app.route('/tobecontinued')
 def continued():
     return render_template('continued.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
