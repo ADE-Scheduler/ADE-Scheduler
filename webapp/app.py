baacc5286f97c5be62095ac4f89c36132cf18380
louisna <louisnavarre@hotmail.com>
2019-08-16 13:02:42 +0200

    removal course corrected

--- webapp/app.py
+++ webapp/app.py
@@ -97,16 +97,17 @@ def getFTS():
 
 
 # To remove the code
-@app.route('/remove/code/<the_code>', methods=['GET'])
+@app.route('/remove/code/<the_code>', methods=['POST'])
 def remove_code(the_code):
     global data_base
     if the_code in codes:
         codes.remove(the_code)
         basic_context['up_to_date'] = False
         data_base.clear()
-        c = getCoursesFromCodes(codes, Q1 + Q2 + Q3, 9)
-        for course in c:
-            data_base += course.getEventsJSON()
+        if len(codes) > 0:
+            c = getCoursesFromCodes(codes, Q1 + Q2 + Q3, 9)
+            for course in c:
+                data_base += course.getEventsJSON()
     return render_template('calendar.html', **basic_context, data_base=json.dumps(data_base), data_sched=json.dumps(data_sched), fts=json.dumps(fts_json))
 
 
