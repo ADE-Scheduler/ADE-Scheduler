98b96872a97750b3f96e17245274031a7c4e17a3
Louis Navarre <louisnavarre@hotmail.com>
2019-09-03 15:02:49 +0200

    add test db + fix API db

---
+++ python/test_db.py
@@ -0,0 +1,27 @@
+from .database import *
+
+def test_basic():
+    dropTables()
+    init()
+    username = 'my_test_username'
+    link = 'my_test_link'
+    settings = 'my_test_settings'
+    setLink(link, username, settings)
+
+    assert isLoginPresent(username)
+    assert isLinkPresent(link)
+
+    link_from_username = getLinkFromUsername(username)
+    assert link_from_username == link
+
+    settings_from_link = getSettingsLink(link)
+    assert settings_from_link == settings
+
+    new_settings = 'my_new_test_settings'
+    updateSettings(link, new_settings)
+    settings_from_link = getSettingsLink(link)
+    assert settings_from_link == new_settings
+
+    deleteLink(link)
+    assert not isLinkPresent(link)
+
