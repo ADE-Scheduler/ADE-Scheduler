728fa7f0c367d398424ca70e07f8678d03e27aa6
Louis Navarre <louisnavarre@hotmail.com>
2019-09-03 17:08:13 +0200

    modify database.py: username is not primary key

--- python/test_db.py
+++ python/test_db.py
@@ -5,7 +5,7 @@ def test_basic():
     init(test=True)
     username = 'my_test_username'
     link = 'my_test_link'
-    settings = 'my_test_settings'
+    settings = {'projectID':9, 'codes':'LINGI2261,LINGI2262'}
     setLink(link, username, settings)
 
     assert isLoginPresent(username)
@@ -14,12 +14,12 @@ def test_basic():
     link_from_username = getLinkFromUsername(username)
     assert link_from_username == link
 
-    settings_from_link = getSettingsLink(link)
+    settings_from_link = getSettingsfromLink(link)
     assert settings_from_link == settings
 
     new_settings = 'my_new_test_settings'
-    updateSettings(link, new_settings)
-    settings_from_link = getSettingsLink(link)
+    updateSettingsFromLink(link, new_settings)
+    settings_from_link = getSettingsfromLink(link)
     assert settings_from_link == new_settings
 
     deleteLink(link)
