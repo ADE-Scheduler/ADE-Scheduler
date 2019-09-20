cc3ca89be551ba500d4c1582aae6a141d0c296c3
Gilles Poncelet <gillesponcelet98@gmail.com>
2019-09-20 22:49:54 +0200

    Event disparition bug fix

--- python/ade.py
+++ python/ade.py
@@ -29,7 +29,8 @@ def get_courses_from_codes(codes, project_id=9):
         else:
             courses.append(loads(course))
 
-    for course in get_courses_from_ade(not_added, project_id, redis=redis):
+    for code in not_added:
+        course = get_courses_from_ade(code, project_id, redis=redis)
         courses.append(course)
         redis.hset(name='Project=' + str(project_id), key=course.code, value=dumps(course))
         redis.expire(course.code, time=timedelta(hours=3))
@@ -37,21 +38,17 @@ def get_courses_from_codes(codes, project_id=9):
     return courses
 
 
-def get_courses_from_ade(codes, project_id, redis=None):
+def get_courses_from_ade(code, project_id, redis=None):
     """
     Fetches courses schedule from UCLouvain's ADE web API
-    :param codes: list of str
+    :param code: str
     :param project_id: int
     :param redis: instance of a Redis server, on which an access token may be stored. If not specified, simply retrieve
                   a new token.
-    :return: list of Courses
+    :return: Course object
     """
-    if not codes:
-        return list()
-
-    # Some variables
-    course_added = []
-    course_list = []
+    # Course to be returned
+    course = None
 
     # We retrieve the access token and construct the URL and the headers for the requests
     if not redis:
@@ -61,35 +58,35 @@ def get_courses_from_ade(codes, project_id, redis=None):
         if not token:
             token, expiry = get_token()
             if expiry > 10:
-                redis.setex('ade_webapi_token', timedelta(seconds=expiry-10), value=token)
+                redis.setex('ade_webapi_token', timedelta(seconds=expiry - 10), value=token)
         else:
             token = token.decode()
     headers = {'Authorization': 'Bearer ' + token}
     url = 'https://api.sgsi.ucl.ac.be:8243/ade/v0/api?login=' + user + '&password=' + password + '&projectId=' + \
           str(project_id) + '&function='
 
-    # We get the ressource IDs for each code
+    # We get the ressource ID
     if not redis or not redis.exists('ade_webapi_id'):
         r = requests.get(url + 'getResources&detail=2', headers=headers)
         root = etree.fromstring(r.content)
         df = DataFrame(data=root.xpath('//resource/@id'), index=map(lambda x: x.upper(), root.xpath('//resource/@name'))
                        , columns=['id'])
         hash_table = df.groupby(level=0).apply(lambda x: '|'.join(x.to_dict(orient='list')['id'])).to_dict()
-        resources_id = '|'.join(filter(None, [hash_table.get(code) for code in codes]))
+        resources_id = '|'.join(filter(None, [hash_table.get(code)]))
         if redis:
             redis.hmset('ade_webapi_id', hash_table)
             redis.expire('ade_webapi_id', timedelta(days=1))
         if not resources_id:
             return list()
     else:
-        result = list(filter(None, redis.hmget('ade_webapi_id', codes)))
+        result = list(filter(None, redis.hmget('ade_webapi_id', code)))
         if result:
             resources_id = '|'.join(map(lambda x: x.decode(), result))
         else:
             return list()
 
     # We get the events
-    r = requests. get(url + 'getActivities&tree=false&detail=17&resources=' + resources_id, headers=headers)
+    r = requests.get(url + 'getActivities&tree=false&detail=17&resources=' + resources_id, headers=headers)
     root = etree.fromstring(r.content)
     for activity in root.xpath('//activity'):
         activity_type = activity.attrib['type']
@@ -99,28 +96,20 @@ def get_courses_from_ade(codes, project_id, redis=None):
             start = event.attrib['startHour']
             end = event.attrib['endHour']
             date = event.attrib['date']
-            event_loc = ''
-            event_prof = ''
-            event_code = ''
-            for participant in event[0]:
-                category = participant.attrib.get('category')
-                if category == 'classroom':
-                    event_loc = participant.attrib['name']
-                elif category == 'instructor':
-                    event_prof = participant.attrib['name']
-                elif category == 'category5':
-                    event_code = participant.attrib['name']
+            event_loc = ' '.join(event.xpath('.//eventParticipant[@category="classroom"]/@name'))
+            event_prof = ' '.join(event.xpath('.//eventParticipant[@category="instructor"]/@name'))
 
-            # We create the event
+            # We create the event and add it to the Course object
             t0, t1 = extractDateTime(date, start, end)
-            event = extractType(activity_type, activity_id)(t0, t1, event_code, activity_name, Professor(event_prof,''),
-                                                            event_loc, id=activity_id)
-            try:  # The course was already added
-                i = course_added.index(event_code)
-                course_list[i].addEvent(event)
-            except ValueError:  # This is a new course
-                course_added.append(event_code)
-                course_list.append(Course(event_code, activity_name))
-                course_list[-1].addEvent(event)
+            event = extractType(activity_type, activity_id)(t0, t1, code, activity_name,
+                                                            Professor(event_prof, ''), event_loc, id=activity_id)
+            if course is None:
+                course = Course(code, activity_name)
+                course.addEvent(event)
+            else:
+                course.addEvent(event)
 
-    return course_list
+    if course is None:
+        return list()
+    else:
+        return course
