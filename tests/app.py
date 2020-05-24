f9bdab0026b7d9a249a2f8d20d7a7a4cba69549d
Gilles Poncelet <gillesponcelet98@gmail.com>
2020-05-24 15:41:52 +0200

    test template

---
+++ tests/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    return render_template('base.html')
+
+
+if __name__ == '__main__':
+    # app.run()
+    app.run(host="10.42.0.1")
