3b87d0708d05ee9099ec1685a9a658eb97cade21
Gilles Poncelet <gillesponcelet98@gmail.com>
2020-08-10 20:01:32 +0200

    Google maps for classroom (TBD, WIP)

--- views/classroom.py
+++ views/classroom.py
@@ -4,11 +4,6 @@ from flask import Blueprint, render_template
 classroom = Blueprint('classroom', __name__, static_folder='../static')
 
 
-@classroom.before_request
-def before_classroom_request():
-    pass
-
-
 @classroom.route('/')
 def index():
     return render_template('classroom.html')
