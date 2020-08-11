0c263bd78de385938da3815bce33109700d5f760
Gilles Poncelet <gillesponcelet98@gmail.com>
2020-08-11 18:29:28 +0200

    Map in classroom page

--- static/js/classroom.js
+++ static/js/classroom.js
@@ -1,24 +1,30 @@
 import Vue from 'vue';
-import * as VueGoogleMaps from 'vue2-google-maps';
+import { Icon, latLng } from 'leaflet';
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
 import './base.js';
 import '../css/classroom.css';
+import 'leaflet/dist/leaflet.css';
 
 
-Vue.use(VueGoogleMaps, {
-    load: {
-        key: 'AIzaSyANRlyAxFvNqYEQ2CtwqTbJwClf9e37pGI',
-        libraries: 'places',
-    },
-    installComponents: true,
+delete Icon.Default.prototype._getIconUrl;
+Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
 });
 
 
 document.addEventListener('DOMContentLoaded', function() {
     var vm = new Vue({
         el: '#app',
-        data: {
-            message: 'Hello from the Classrooms page ! :-)'
-        },
         delimiters: ['[[',']]'],
+        components: { LMap, LTileLayer, LMarker },
+        data: {
+            url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            center: latLng(50.6681, 4.6118),
+            zoom: 15,
+            markers: [latLng(50.6681, 4.6118)],
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+        },
     });
 });
--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,16 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin')
 module.exports = {
     mode: 'development',
     entry: {
-        welcome: "./static/js/welcome.js",
-        calendar: "./static/js/calendar.js",
-        account: "./static/js/account.js",
-        classroom: "./static/js/classroom.js",
-        help: "./static/js/help.js",
-        change_password: "./static/js/security/change_password.js",
-        forgot_password: "./static/js/security/forgot_password.js",
-        login_user: "./static/js/security/login_user.js",
-        register_user: "./static/js/security/register_user.js",
-        reset_password: "./static/js/security/reset_password.js",
+        welcome: './static/js/welcome.js',
+        calendar: './static/js/calendar.js',
+        account: './static/js/account.js',
+        classroom: './static/js/classroom.js',
+        help: './static/js/help.js',
+        change_password: './static/js/security/change_password.js',
+        forgot_password: './static/js/security/forgot_password.js',
+        login_user: './static/js/security/login_user.js',
+        register_user: './static/js/security/register_user.js',
+        reset_password: './static/js/security/reset_password.js',
     },
     resolve: {
         alias: {
@@ -21,6 +21,17 @@ module.exports = {
     },
     module: {
         rules: [
+            {
+                test: /\.(png|jpe?g|gif|svg)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[contenthash].[ext]',
+                        outputPath: 'img',
+                        esModule: false
+                    }
+                }]
+            },
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
@@ -35,7 +46,8 @@ module.exports = {
         new VueLoaderPlugin()
     ],
     output: {
-        path: __dirname + "/static/dist",
-        filename: "[name].bundle.js"
+        path: __dirname + '/static/dist',
+        filename: '[name].bundle.js',
+        publicPath: '/static/dist/'
     },
 }
