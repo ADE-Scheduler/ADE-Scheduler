250a9fb8799dd36822723ba8b75c0f93e8de1c2c
Eertmans <jeertmans@icloud.com>
2020-08-16 16:54:27 +0200

    du coup cela marche mtn

--- app.py
+++ app.py
@@ -131,15 +131,10 @@ def set_locale(locale):
 @app.before_first_request
 def before_first_request():
     # TODO: Apache il aime pas (a pas la permission de write I guess)
-    # Fix temporaire: copier/coller le fichier sur le serv à chaque fois qu'on change un des pathname/view !
-    # Autre soucis: il se cache dans les webnav, faut changer la version pour éviter que ça ne se re-fetch jamais
-    """
     if not os.path.exists('static/dist'):
         os.makedirs('static/dist')
     with open('static/dist/jsglue.min.js', 'w') as f:
         f.write(jsmin(jsglue.generate_js()))
-    """
-    pass
 
 
 # Reset current schedule on user logout
