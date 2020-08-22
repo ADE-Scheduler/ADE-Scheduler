ed162d27424fb596408d016145f213573e18fbcf
Gilles Poncelet <gillesponcelet98@gmail.com>
2020-08-22 16:13:38 +0200

    Add messages to CLI's commands

--- cli.py
+++ cli.py
@@ -1,5 +1,6 @@
 import random as rnd
 import click
+from datetime import datetime
 from flask import current_app as app
 from flask.cli import with_appcontext
 
@@ -34,6 +35,7 @@ def update(with_appcontext=False):
     mng.update_resources()
     mng.update_course_resources()
     mng.update_classrooms()
+    click.echo(f'Successfully updated the tables on {datetime.now().strftime("%d/%m/%Y at %H:%M:%S")}.')
 
 
 @redis.command()
@@ -42,6 +44,7 @@ def flush():
     """Resets Redis, deleting all keys."""
     mng = app.config['MANAGER']
     mng.server.flushdb()
+    click.echo('Sucessfully flushed Redis.')
 
 
 @redis.command()
@@ -53,13 +56,21 @@ def expire(pattern, time, random):
     """Apply an expiry time to keys in Redis."""
     rd = app.config['MANAGER'].server
 
-    for key in rd.scan_iter(match=f'{pattern}'):
-        if random:
-            rd.expire(key, rnd.randint(200, 400))
-        elif time > 0:
+    if random:
+        def f(key):
+            key: rd.expire(key, rnd.randint(200, 400))
+    elif time > 0:
+        def f(key):
             rd.expire(key, time)
-        else:
+    else:
+        def f(key):
             rd.delete(key)
+
+    i = 0
+    for key in rd.scan_iter(match=f'{pattern}'):
+        f(key)
+        i += 1
+    click.echo(f'Successfully applied expire to {i} keys.')
 
 
 @redis.command()
