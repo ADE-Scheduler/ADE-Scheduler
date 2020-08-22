9fe6372c42cd987ff001fec432e7b102b8d33568
Gilles Poncelet <gillesponcelet98@gmail.com>
2020-08-22 19:14:54 +0200

    Add user CLI group + proper v1 for Migrations

---
+++ migrations/versions/1f13a87097d8_db_v1.py
@@ -0,0 +1,24 @@
+"""db_v1
+
+Revision ID: 1f13a87097d8
+Revises: 
+Create Date: 2020-08-22 19:08:44.731191
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1f13a87097d8'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
