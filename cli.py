1409b254868201e48b16024991ad53c8954e92fe
Gilles Poncelet <gillesponcelet98@gmail.com>
2020-08-22 20:06:43 +0200

    Add some nice colors

--- cli.py
+++ cli.py
@@ -19,7 +19,7 @@ def renew():
     """Renews client's token."""
     cli = app.config['MANAGER'].client
     cli.renew_token()
-    click.echo(f'Token successfully renewed. It will expiry in {cli.expire_in():.1f} seconds.')
+    click.secho(f'Token successfully renewed. It will expiry in {cli.expire_in():.1f} seconds.', fg='green')
 
 
 @client.command()
@@ -46,7 +46,7 @@ def update(with_appcontext=False):
     mng.update_resources()
     mng.update_course_resources()
     mng.update_classrooms()
-    click.echo(f'Successfully updated the tables on {datetime.now().strftime("%d/%m/%Y at %H:%M:%S")}.')
+    click.secho(f'Successfully updated the tables on {datetime.now().strftime("%d/%m/%Y at %H:%M:%S")}.', fg='green')
 
 
 @redis.command()
@@ -55,7 +55,7 @@ def flush():
     """Resets Redis, deleting all keys."""
     mng = app.config['MANAGER']
     mng.server.flushdb()
-    click.echo('Sucessfully flushed Redis.')
+    click.secho('Sucessfully flushed Redis.', fg='green')
 
 
 @redis.command()
@@ -81,7 +81,7 @@ def expire(pattern, time, random):
     for key in rd.scan_iter(match=f'{pattern}'):
         f(key)
         i += 1
-    click.echo(f'Successfully applied expire to {i} keys.')
+    click.secho(f'Successfully applied expire to {i} keys.', fg='green')
 
 
 @redis.command()
