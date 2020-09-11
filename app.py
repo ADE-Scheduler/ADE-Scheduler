ba219e242ff1b1c6fa6e4cc639ef395134472694
Gilles Poncelet <gillesponcelet98@gmail.com>
2020-09-11 10:16:54 +0200

    Change mail parameters (server name in .flaskenv)

--- app.py
+++ app.py
@@ -75,8 +75,8 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ['MAIL_USERNAME']
 app.config['MAIL_PASSWORD'] = os.environ['MAIL_PASSWORD']
-app.config['MAIL_DEFAULT_SENDER'] = 'no-reply@' + app.config['MAIL_SERVER']
-app.config['ADMINS'] = [os.environ['MAIL_USERNAME']]
+app.config['MAIL_DEFAULT_SENDER'] = os.environ['MAIL_USERNAME']
+app.config['ADMINS'] = [os.environ['MAIL_ADMIN']]
 app.config['MAIL_MANAGER'] = Mail(app)
 
 # Setup Flask-SQLAlchemy
