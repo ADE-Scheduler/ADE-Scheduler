ccb0df845609140fe491772c191ca8c6dd8f9be7
Gilles Poncelet <gillesponcelet98@gmail.com>
2020-09-18 15:08:39 +0200

    Color picker on main calendar page (#58)
    * First proposition of a color picker on the calendar page
    
    * Backend support for calendar color picker
    
    * move code line
    
    Co-authored-by: Gilles Poncelet <gillesponcelet98@gmail.com>

--- views/calendar.py
+++ views/calendar.py
@@ -33,7 +33,7 @@ class CalendarDecoder(json.JSONDecoder):
         decoded = json.JSONDecoder().decode(obj)
         for key in decoded:
             obj = decoded[key]
-            if isinstance(obj, list) and isinstance(obj[0], str):
+            if isinstance(obj, list) and isinstance(obj[0], str) and key != 'color_palette':
                 decoded[key] = set(obj)
         return decoded
 
@@ -75,6 +75,7 @@ def clear():
         'current_schedule': {
             'id': session['current_schedule'].id,
             'label': _(session['current_schedule'].label),
+            'color_palette': session['current_schedule'].color_palette,
         },
         'current_project_id': session['current_schedule'].project_id,
     }), 200
@@ -90,6 +91,7 @@ def get_data():
         'current_schedule': {
             'id': session['current_schedule'].id,
             'label': _(session['current_schedule'].label),
+            'color_palette': session['current_schedule'].color_palette,
         },
         'n_schedules': len(session['current_schedule'].best_schedules),
         'events': session['current_schedule'].get_events(json=True),
@@ -113,7 +115,7 @@ def load_schedule(id):
             'current_schedule': {
                 'id': schedule.data.id,
                 'label': _(schedule.data.label),
-                'project_id': schedule.data.project_id,
+                'color_palette': schedule.data.color_palette,
             },
             'project_id': mng.get_project_ids(),
             'current_project_id': session['current_schedule'].project_id,
@@ -330,4 +332,16 @@ def compute():
         'n_schedules': len(session['current_schedule'].best_schedules) if bests is not None else 0,
         'events': session['current_schedule'].get_events(json=True, schedule_number=1) if bests is not None else list(),
         'selected_schedule': 1 if bests is not None else 0
+    }), 200
+
+
+@calendar.route('/schedule/color', methods=['POST'])
+def update_color():
+    color_palette = request.json.get('color_palette')
+    if color_palette:
+        session['current_schedule'].color_palette = color_palette
+
+    schedule_number = int(request.json.get('schedule_number'))
+    return jsonify({
+        'events': session['current_schedule'].get_events(json=True, schedule_number=schedule_number),
     }), 200
