75e320d40abfb5178cc107eede0bdc930405ed25
louisna <37408317+louisna@users.noreply.github.com>
2020-09-23 09:52:28 +0200

    What is new (#67)
    * Add templates for 'what is new' page
    
    * Add page link in base menu
    
    * Example de kalitey
    
    * Text 'Nouveautés'
    
    * Translations for what is new page

---
+++ static/js/whatisnew.js
@@ -0,0 +1,40 @@
+import Vue from 'vue';
+import { Collapse, ScrollSpy } from 'bootstrap';
+import './base.js';
+import '../css/whatisnew.css';
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var vm = new Vue({
+        el: '#app',
+        data: {
+            navBtn: false,
+        },
+        delimiters: ['[[',']]'],
+
+        methods: {
+            scroll: function(id, flag) {
+                document.getElementById(id).scrollIntoView();
+                if (!((window.innerHeight + window.scrollY) >= document.body.offsetHeight)) {
+                    window.scrollBy(0, -70);
+                }
+                if (window.innerWidth < 767.98 && !flag) {
+                    this.toggleNav(false);
+                }
+            },
+            toggleNav: function(show) {
+                this.navBtn = show;
+                if (show)   { nav.show(); }
+                else        { nav.hide(); }
+            },
+        },
+    });
+
+    var scrollSpy = new ScrollSpy(document.body, {
+        target: '#faq-navigator',
+        offset: 70
+    });
+    var nav = new Collapse(document.getElementById('faq-navigator'), {
+        toggle: false,
+    });
+});
---
+++ views/whatisnew.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, render_template
+
+
+whatisnew = Blueprint('whatisnew', __name__, static_folder='../static')
+
+
+@whatisnew.route('/')
+def index():
+    return render_template('whatisnew.html')
