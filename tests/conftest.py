0c2a236ca3d28b34aefa51ca3487c8d47dc0c78d
Gilles Poncelet <gillesponcelet98@gmail.com>
2020-10-09 21:44:20 +0200

    Initialise unit tests

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+import os
+import sys
+import pytest
+import tempfile
+from dotenv import load_dotenv, find_dotenv
+
+sys.path.append(os.getcwd())
+load_dotenv(find_dotenv('.flaskenv'))
+
+from app import app as ade_scheduler
+
+@pytest.fixture
+def app():
+    return ade_scheduler
