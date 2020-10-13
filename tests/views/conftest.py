1ef82e38c5a48cf13c38e939cc3c053a628b7a10
Gilles Poncelet <gillesponcelet98@gmail.com>
2020-10-13 21:11:40 +0200

    add first steps to create user

--- tests/views/conftest.py
+++ tests/views/conftest.py
@@ -1,7 +1,23 @@
 import pytest
+import backend.models as md
+
 from app import app as ade_scheduler
 
 
 @pytest.fixture
 def app():
     return ade_scheduler
+
+
+@pytest.fixture
+def user():
+    test_users = list()
+    test_users.append(md.User(email="jyl@scheduler.ade", password="password"))
+    # Add schedules, etc. to users for testing purposes
+    md.db.session.commit()
+
+    yield test_users
+
+    for user in test_users:
+        user.delete()
+    md.db.session.commit()
