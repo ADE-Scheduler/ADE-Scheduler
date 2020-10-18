d644faa9eaa21b34d3aee28271953d2669e5498a
Gilles Poncelet <gillesponcelet98@gmail.com>
2020-10-18 13:18:50 +0200

    fix user error on schedule choice

--- views/calendar.py
+++ views/calendar.py
@@ -302,7 +302,13 @@ def save():
 def download():
     mng = app.config["MANAGER"]
     link = request.args.get("link")
-    choice = int(request.args.get("choice")) if request.args.get("choice") else 0
+
+    try:
+        choice = int(request.args.get("choice")) if request.args.get("choice") else 0
+    except ValueError as e:
+        choice = 0
+        # No comment... long live the user !
+
     if link:
         schedule = mng.get_schedule(link)[0]
     else:
