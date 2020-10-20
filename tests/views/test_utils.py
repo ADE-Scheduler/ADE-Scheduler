443b9893d310e51bf2d7de80f60ec74eb0604122
Gilles Poncelet <gillesponcelet98@gmail.com>
2020-10-20 22:47:26 +0200

    Add tests for views.utils

---
+++ tests/views/test_utils.py
@@ -0,0 +1,19 @@
+import views.utils as utl
+
+
+def test_init_session():
+    """Test the session initialisation"""
+    # TODO
+    assert True
+
+
+def test_autosave_schedule():
+    """Test the schedule autosave"""
+    # TODO
+    assert True
+
+
+def test_autoload_schedule():
+    """Test the schedule autoload"""
+    # TODO
+    assert True
