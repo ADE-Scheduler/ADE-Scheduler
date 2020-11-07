fb180f2c521764c7ec2bce406a20e3a34b84183c
Gilles Poncelet <gillesponcelet98@gmail.com>
2020-11-07 10:25:01 +0100

    Make contact, help & whatisnew accessible

--- views/whatisnew.py
+++ views/whatisnew.py
@@ -4,11 +4,6 @@ from flask import Blueprint, render_template
 whatisnew = Blueprint("whatisnew", __name__, static_folder="../static")
 
 
-@whatisnew.before_request
-def before_whatisnew_request():
-    utl.init_session()
-
-
 @whatisnew.route("/")
 def index():
     return render_template("whatisnew.html")
