eed134069ef00cfad09b87ee91c0b399eacdf76d
Gilles Poncelet <gillesponcelet98@gmail.com>
2020-11-17 11:27:16 +0100

    move in other files

---
+++ cli/usage.py
@@ -0,0 +1,43 @@
+import click
+import pandas as pd
+import backend.models as md
+
+from flask import current_app as app
+from flask.cli import with_appcontext
+from cli.cli import usage
+
+
+@usage.command()
+@with_appcontext
+def stats():
+    threshold = 100
+    pd.set_option("display.max_rows", None)
+    pd.set_option("display.max_columns", None)
+
+    click.echo("Reading datase...")
+    df = md.table_to_dataframe(md.Usage, columns=["url", "speed", "blueprint"])
+
+    with app.test_request_context():
+        host_url = flask.request.host_url
+
+    host_url = "https://ade-scheduler.info.ucl.ac.be/"
+
+    click.echo(
+        f"Computing all requests matching {host_url}<blueprint>/<request>)"
+        f" and appearing at least {threshold: d} times..."
+    )
+
+    endpoints = (
+        df["url"]
+        .str.replace(host_url[:-1], "")
+        .str.extract(r"^\/\w+\/(\w+)", expand=False)
+    )
+    df["url"] = endpoints
+    req_stats = (
+        df.groupby(["blueprint", "url"])
+        .filter(lambda x: len(x) > threshold)
+        .groupby(["blueprint", "url"])
+    )
+
+    click.echo(req_stats.speed.describe())
+    click.echo(host_url)
