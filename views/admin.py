88eabe84802781d6a5d1ca75a80292773a261936
Gilles Poncelet <gillesponcelet98@gmail.com>
2020-11-17 12:03:07 +0100

    add role requiremetns -- doesn't work

--- views/admin.py
+++ views/admin.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, render_template
-
+from flask_security import roles_required
 
 admin = Blueprint("admin", __name__, static_folder="../static")
 
 
+@roles_required("admin")
 @admin.route("/")
 def index():
     return render_template("admin.html")
