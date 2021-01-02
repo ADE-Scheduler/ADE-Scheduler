22fda96568830cb3da76161673b3c2891253144e
Gilles Poncelet <gillesponcelet98@gmail.com>
2021-01-02 13:33:43 +0100

    Add ESLint for JS

--- static/js/admin.js
+++ static/js/admin.js
@@ -1,5 +1,7 @@
+/* global Flask */
+
 import Vue from 'vue';
-import { Carousel } from 'bootstrap'
+import { Carousel } from 'bootstrap';   // eslint-disable-line no-unused-vars
 
 import './base.js';
 import '../css/admin.css';
@@ -9,47 +11,47 @@ const axios = require('axios');
 
 
 document.addEventListener('DOMContentLoaded', function() {
-    var vm = new Vue({
-        el: '#app',
-        delimiters: ['[[',']]'],
-        data: {
-            error: false,
-            computing: false,
-            plots: [],
-        },
-        methods: {
-            fetchData: function() {
-                this.computing = true;
-                axios({
-                    method: 'GET',
-                    url: Flask.url_for('admin.get_data'),
-                })
-                .then(resp => {
-                    this.plots = resp.data;
-                    this.$nextTick(() => {
-                        this.plots.forEach(plot => {
-                            let obj = JSON.parse(plot.data);
-                            obj.layout.width = document.getElementById('carouselPlots').offsetWidth-320;
-                            obj.layout.height = obj.layout.width * 0.7;
-                            Plotly.newPlot(plot.id, obj);
-                        });
-                    });
-                })
-                .catch(err => {
-                    this.error = true;
-                })
-                .then(() => {
-                    this.computing = false;
-                });
-            },
-        },
-        created:  function () {
-            this.fetchData();
-        },
-        computed: {
-            opacity: function() {
-                return {'opacity': this.computing ? '0.2':'1'}
-            },
-        },
-    });
+  new Vue({
+    el: '#app',
+    delimiters: ['[[',']]'],
+    data: {
+      error: false,
+      computing: false,
+      plots: [],
+    },
+    methods: {
+      fetchData: function() {
+        this.computing = true;
+        axios({
+          method: 'GET',
+          url: Flask.url_for('admin.get_data'),
+        })
+          .then(resp => {
+            this.plots = resp.data;
+            this.$nextTick(() => {
+              this.plots.forEach(plot => {
+                let obj = JSON.parse(plot.data);
+                obj.layout.width = document.getElementById('carouselPlots').offsetWidth-320;
+                obj.layout.height = obj.layout.width * 0.7;
+                Plotly.newPlot(plot.id, obj);
+              });
+            });
+          })
+          .catch(() => {
+            this.error = true;
+          })
+          .then(() => {
+            this.computing = false;
+          });
+      },
+    },
+    created:  function () {
+      this.fetchData();
+    },
+    computed: {
+      opacity: function() {
+        return {'opacity': this.computing ? '0.2':'1'};
+      },
+    },
+  });
 });
