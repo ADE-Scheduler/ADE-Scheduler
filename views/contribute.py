c37d61e666452e878545e261606697376a04c10c
Eertmans <jeertmans@icloud.com>
2021-01-23 18:48:14 +0100

    Init new contribution page

---
+++ views/contribute.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, render_template
+
+
+contribute = Blueprint("contribute", __name__, static_folder="../static")
+
+
+@contribute.route("/")
+def index():
+    return render_template("contribute.html")
