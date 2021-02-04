ac31c7192160340e936ec1ab1b990968927c638b
Jerome Eertmans <jeertmans@icloud.com>
2021-02-04 19:47:15 +0100

    returns top process usage, mem or cpu are optional to trigger sorting

--- bot/bot.py
+++ bot/bot.py
@@ -81,6 +81,43 @@ def status_command(update: Update, context: CallbackContext):
 
 
 @admin_required
+def top_command(update: Update, context: CallbackContext):
+    """
+    Display processes usage using top tool.
+    """
+    commands = ["mem", "cpu"]
+
+    # Load args
+    try:
+        command = context.args[0]
+
+        if command not in commands:
+            update.message.reply_text("Unknown command !")
+            return
+
+    except (IndexError, ValueError):
+        command = "mem"
+
+    res = subprocess.run(
+        ["ps", "-o", "%cpu,%mem,comm", "ax", f"--sort=-%{command}"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+
+    res = subprocess.run(
+        ["head", "-20"],
+        input=res.stdout,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+
+    if res.stdout:
+        update.message.reply_markdown_v2(f"```\n{res.stdout.decode()}\n```")
+    if res.stderr:
+        update.message.reply_markdown_v2(f"```\n{res.stderr.decode()}\n```")
+
+
+@admin_required
 def flask_command(update: Update, context: CallbackContext):
     """
     Interact with the Flask CLI.
@@ -134,9 +171,10 @@ job_queue = updater.job_queue
 dispatcher.add_handler(CommandHandler("service", service_command))
 dispatcher.add_handler(CommandHandler("status", status_command))
 dispatcher.add_handler(CommandHandler("flask", flask_command))
+dispatcher.add_handler(CommandHandler("top", top_command))
 
 # Repeating tasks
-job_queue.run_repeating(health_check, interval=60, first=10)
+# job_queue.run_repeating(health_check, interval=60, first=10)
 
 # Start bot
 updater.start_polling()
