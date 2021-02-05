315bbbb99e7afe8e9024b0e379616db279955a1d
Gilles Poncelet <gillesponcelet98@gmail.com>
2021-02-05 23:20:47 +0100

    different vue linter req

--- components/ScrollSpy.vue
+++ components/ScrollSpy.vue
@@ -2,30 +2,24 @@
   <div>
     <div class="row">
       <!-- Menu bar -->
-      <nav
-        id="navigator"
-        class="col-md-3 col-lg-2 d-md-block bg-light navigator collapse"
-      >
+      <nav class="col-md-3 col-lg-2 d-md-block bg-light navigator collapse" id="navigator">
         <nav class="nav nav-pills navigator-sticky d-flex flex-column p-3">
-          <div
-            v-for="item in content"
-            :key="item.id"
-          >
-            <a
-              class="nav-link main-link mb-0 active"
-              :href="'#' + item.id"
-              @click.prevent="scroll(item.id, true)"
-              v-html="item.title"
-            />
+          <div v-for="item in content" v-bind:key="item.id">
+            <a class="nav-link main-link mb-0 active"
+               v-bind:href="'#' + item.id"
+               v-on:click.prevent="scroll(item.id, true)"
+               v-html="item.title"
+            >
+            </a>
             <nav class="nav sub-nav flex-column">
-              <a
-                v-for="subitem in item.subtitles"
-                :key="subitem.id"
-                class="nav-link ms-4"
-                :href="'#' + subitem.id"
-                @click="scroll(subitem.id, false)"
-                v-html="subitem.title"
-              />
+              <a class="nav-link ms-4"
+                 v-for="subitem in item.subtitles"
+                 v-bind:key="subitem.id"
+                 v-bind:href="'#' + subitem.id"
+                 v-on:click="scroll(subitem.id, false)"
+                 v-html="subitem.title"
+              >
+              </a>
             </nav>
           </div>
         </nav>
@@ -34,39 +28,19 @@
       <!-- Content -->
       <div class="col-md-9 ms-sm-auto col-lg-10 p-3">
         <div class="container-lg">
-          <div
-            v-for="item in content"
-            :key="item.id"
-          >
+          <div v-for="item in content" v-bind:key="item.id">
             <div class="d-flex flex-row my-5">
-              <h3
-                v-show="item.icon"
-                class="align-item-center me-2"
-                :class="item.icon"
-              />
-              <h3
-                :id="item.id"
-                v-html="item.title"
-              />
+              <h3 class="align-item-center me-2" v-bind:class="item.icon" v-show="item.icon"></h3>
+              <h3 v-bind:id="item.id" v-html="item.title"></h3>
             </div>
-            <p v-html="item.content" />
-            <div
-              v-for="subitem in item.subtitles"
-              :key="subitem.id"
-            >
+            <p v-html="item.content"></p>
+            <div v-for="subitem in item.subtitles" v-bind:key="subitem.id">
               <div class="d-flex flex-row">
-                <h4
-                  v-show="subitem.icon"
-                  class="align-item-center me-2"
-                  :class="subitem.icon"
-                />
-                <h4
-                  :id="subitem.id"
-                  v-html="subitem.title"
-                />
+                <h4 class="align-item-center me-2" v-bind:class="subitem.icon" v-show="subitem.icon"></h4>
+                <h4 v-bind:id="subitem.id" v-html="subitem.title"></h4>
               </div>
               <hr class="mb-3">
-              <p v-html="subitem.content" />
+              <p v-html="subitem.content"></p>
             </div>
           </div>
         </div>
@@ -74,60 +48,49 @@
     </div>
 
     <!-- Toggle button  -->
-    <button
-      class="btn btn-dark btn-nav"
-      @click="toggleNav"
-    >
-      <i
-        v-if="navBtn"
-        style="font-size: 32px"
-        class="bi bi-chevron-up"
-      />
-      <i
-        v-else
-        style="font-size: 32px"
-        class="bi bi-chevron-down"
-      />
+    <button class="btn btn-dark btn-nav" v-on:click="toggleNav">
+      <i style="font-size: 32px" class="bi bi-chevron-up" v-if="navBtn"/>
+      <i style="font-size: 32px" class="bi bi-chevron-down" v-else/>
     </button>
   </div>
 </template>
 
 
 <script>
-import { Collapse, ScrollSpy } from 'bootstrap';
+  import { Collapse, ScrollSpy } from 'bootstrap';
 
-export default {
+  export default {
   props: ['content'],
-  data() {
-    return {
-      nav: {},
-      navBtn: false,
-    };
-  },
-  mounted() {
-    console.log(document.getElementById('navigator').classList);
-    this.nav = new Collapse(document.getElementById('navigator'), {
-      toggle: false,
-    });
-    new ScrollSpy(document.body, {
-      target: '#navigator',
-      offset: 70,
-    });
-  },
-  methods: {
-    scroll(id, flag) {
-      document.getElementById(id).scrollIntoView();
-      if (window.innerWidth < 767.98 && !flag) {
-        this.toggleNav();
+    data() {
+      return {
+        nav: {},
+        navBtn: false,
       }
     },
-    toggleNav() {
-      this.navBtn = !this.navBtn;
-      if (this.navBtn)  { this.nav.show(); }
-      else              { this.nav.hide(); }
+    methods: {
+      scroll(id, flag) {
+        document.getElementById(id).scrollIntoView();
+        if (window.innerWidth < 767.98 && !flag) {
+          this.toggleNav();
+        }
+      },
+      toggleNav() {
+        this.navBtn = !this.navBtn;
+        if (this.navBtn)  { this.nav.show(); }
+        else              { this.nav.hide(); }
+      },
     },
+    mounted() {
+      console.log(document.getElementById('navigator').classList)
+      this.nav = new Collapse(document.getElementById('navigator'), {
+        toggle: false,
+      });
+      new ScrollSpy(document.body, {
+        target: '#navigator',
+        offset: 70,
+      });
+    }
   }
-};
 </script>
 
 
