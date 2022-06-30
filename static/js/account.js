a22f8e2deab4c7d116ed9aec306e4d1bdd73b879
Corentin <corentin.libert@student.uclouvain.be>
2022-06-30 19:23:31 +0200

    Delete External Activity on button

--- static/js/account.js
+++ static/js/account.js
@@ -1,32 +1,31 @@
 /* global Flask */
 
-import Vue from 'vue';
-import store from './store.js';
-import Spinner from '../../components/Spinner.vue';
-import VSwatches from 'vue-swatches';
-import { Modal } from 'bootstrap';
-import './base.js';
-import 'vue-swatches/dist/vue-swatches.css';
-import '../css/account.css';
-const axios = require('axios');
+import Vue from "vue";
+import store from "./store.js";
+import Spinner from "../../components/Spinner.vue";
+import VSwatches from "vue-swatches";
+import { Modal } from "bootstrap";
+import "./base.js";
+import "vue-swatches/dist/vue-swatches.css";
+import "../css/account.css";
+const axios = require("axios");
 
-
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function() {
   new Vue({
-    el: '#app',
-    delimiters: ['[[',']]'],
-    components: { VSwatches, 'spinner': Spinner },
+    el: "#app",
+    delimiters: ["[[", "]]"],
+    components: { VSwatches, spinner: Spinner },
     data: function() {
       return {
         projectId: [],
         schedules: [],
         externalActivities: [],
         currentSchedule: {},
-        labelBackup: '',
+        labelBackup: "",
         computing: true,
         unsaved: true,
         autoSave: false,
-        isEditing: false,
+        isEditing: false
       };
     },
     created: function() {
@@ -36,11 +35,11 @@ document.addEventListener('DOMContentLoaded', function() {
       fetchData: function() {
         this.computing = true;
         axios({
-          method: 'GET',
-          url: Flask.url_for('account.get_data'),
+          method: "GET",
+          url: Flask.url_for("account.get_data")
         })
           .then(resp => {
-            this.externalActivities = resp.data.external_activities; 
+            this.externalActivities = resp.data.external_activities;
             this.projectId = resp.data.project_id;
             this.unsaved = resp.data.unsaved;
             this.schedules = resp.data.schedules;
@@ -55,13 +54,13 @@ document.addEventListener('DOMContentLoaded', function() {
             this.computing = false;
           });
       },
-      changeAutoSave: function () {
+      changeAutoSave: function() {
         this.computing = true;
         axios({
-          method: 'POST',
-          url: Flask.url_for('account.autosave'),
-          header: {'Content-Type': 'application/json'},
-          data: { autosave: this.autoSave },
+          method: "POST",
+          url: Flask.url_for("account.autosave"),
+          header: { "Content-Type": "application/json" },
+          data: { autosave: this.autoSave }
         })
           .then(() => {})
           .catch(err => {
@@ -75,8 +74,8 @@ document.addEventListener('DOMContentLoaded', function() {
         this.request = function() {
           this.computing = true;
           axios({
-            method: 'GET',
-            url: Flask.url_for('account.load_schedule', {'id': id}),
+            method: "GET",
+            url: Flask.url_for("account.load_schedule", { id: id })
           })
             .then(resp => {
               this.unsaved = resp.data.unsaved;
@@ -100,14 +99,16 @@ document.addEventListener('DOMContentLoaded', function() {
       viewSchedule: function(e, id) {
         // ...
         this.request = function() {
-          if (id == null) { id = -1; }
+          if (id == null) {
+            id = -1;
+          }
           this.computing = true;
           axios({
-            method: 'GET',
-            url: Flask.url_for('account.load_schedule', {'id': id}),
+            method: "GET",
+            url: Flask.url_for("account.load_schedule", { id: id })
           })
             .then(() => {
-              window.location.href = Flask.url_for('calendar.index');
+              window.location.href = Flask.url_for("calendar.index");
             })
             .catch(err => {
               store.error(err.response.data);
@@ -124,11 +125,13 @@ document.addEventListener('DOMContentLoaded', function() {
         }
       },
       deleteSchedule: function(e, id) {
-        if (id == null) { id = -1; }
+        if (id == null) {
+          id = -1;
+        }
         this.computing = true;
         axios({
-          method: 'DELETE',
-          url: Flask.url_for('account.delete_schedule', {'id': id}),
+          method: "DELETE",
+          url: Flask.url_for("account.delete_schedule", { id: id })
         })
           .then(resp => {
             let index = this.schedules.findIndex(s => s.id === id);
@@ -146,20 +149,16 @@ document.addEventListener('DOMContentLoaded', function() {
           });
       },
       deleteExternalActivity: function(e, id) {
-        if (id == null) { id = -1; }
+        if (id == null) {
+          id = -1;
+        }
         this.computing = true;
         axios({
-          method: 'DELETE',
-          url: Flask.url_for('account.delete_external_activity', {'id': id}),
+          method: "DELETE",
+          url: Flask.url_for("account.delete_external_activity", { id: id })
         })
           .then(resp => {
-            //TODO : delete external activity
-            let index = this.schedules.findIndex(s => s.id === id);
-            if (index > -1) {
-              this.schedules.splice(index, 1);
-            }
-            this.currentSchedule = resp.data.current_schedule;
-            this.labelBackup = this.currentSchedule.label;
+            location.reload();
           })
           .catch(err => {
             store.error(err.response.data);
@@ -171,12 +170,14 @@ document.addEventListener('DOMContentLoaded', function() {
       updateLabel: function(id) {
         // ...
         this.computing = true;
-        if (id == null) { id = -1; }
+        if (id == null) {
+          id = -1;
+        }
         axios({
-          method: 'PATCH',
-          url: Flask.url_for('account.update_label', {'id': id}),
-          header: {'Content-Type': 'applacation/json'},
-          data: {'label': this.currentSchedule.label},
+          method: "PATCH",
+          url: Flask.url_for("account.update_label", { id: id }),
+          header: { "Content-Type": "applacation/json" },
+          data: { label: this.currentSchedule.label }
         })
           .then(() => {
             this.isEditing = false;
@@ -196,13 +197,17 @@ document.addEventListener('DOMContentLoaded', function() {
       save: function() {
         this.computing = true;
         axios({
-          method: 'POST',
-          url: Flask.url_for('account.save'),
-          header: {'Content-Type': 'applacation/json'},
-          data: this.currentSchedule,
+          method: "POST",
+          url: Flask.url_for("account.save"),
+          header: { "Content-Type": "applacation/json" },
+          data: this.currentSchedule
         })
           .then(resp => {
-            if (this.schedules.findIndex(s => s.id === resp.data.saved_schedule.id) < 0) {
+            if (
+              this.schedules.findIndex(
+                s => s.id === resp.data.saved_schedule.id
+              ) < 0
+            ) {
               this.schedules.push(resp.data.saved_schedule);
               this.currentSchedule.id = resp.data.saved_schedule.id;
             }
@@ -215,10 +220,12 @@ document.addEventListener('DOMContentLoaded', function() {
             this.computing = false;
           });
       },
-      warningContinue: function() {this.request();},
-      request: function() {},
-    },
+      warningContinue: function() {
+        this.request();
+      },
+      request: function() {}
+    }
   });
 
-  var warningModal = new Modal(document.getElementById('warningModal'));
+  var warningModal = new Modal(document.getElementById("warningModal"));
 });
